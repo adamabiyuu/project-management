1b6df411b1c4ce0ebf5f48945b6be27c42473d5b
adamabiyuu <advmabiyu@gmail.com>
2026-07-08 12:53:46 +0700

    feat: Add migrations for cards, comments, labels, assignees, and attachments

---
+++ repositories/card_repository.go
@@ -0,0 +1,32 @@
+package repositories
+
+import (
+	"github.com/adamabiyuu/project-management/config"
+	"github.com/adamabiyuu/project-management/models"
+)
+
+type CardRepository interface {
+	Create(card *models.Card) error
+	Update(card *models.Card) error
+	Delete(id uint) error
+}
+
+type cardRepository struct {
+
+}
+
+func NewCardRepository() CardRepository {
+	return &cardRepository{}
+}
+
+func (r *cardRepository) Create(card *models.Card) error {
+	return config.DB.Create(card).Error
+}
+
+func (r *cardRepository) Update(card *models.Card) error {
+	return config.DB.Save(card).Error
+}
+
+func (r *cardRepository) Delete(id uint) error {
+	return config.DB.Delete(&models.Card{}, id).Error
+}
